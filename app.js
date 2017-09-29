3ff9043d47468318406993c24dcbfef9d15ff7c1
Bill Odell <billodell@Bills-MacBook-Pro.local>
2017-09-28 21:09:00 -0700

    removed function

--- app.js
+++ app.js
@@ -2,15 +2,6 @@
 
 var score = 0;
 
-function question(val){
-  if(val === 'yes' || val === 'y') {
-    score++;
-    alert('Congrats, you have ' + score + ' points');
-  } else {
-    alert('Sorry :(, better luck on the next one!');
-  }
-}
-
 console.log('Initialized properly');
 alert('Welcome to my guessing game');
 
@@ -20,25 +11,45 @@ console.log('users name ' + user);
 
 var colorBlue = prompt('Is my favorite color blue?').toLowerCase();
 console.log('favorite color guess ' + colorBlue);
-
-question (colorBlue);
+if(colorBlue === 'yes' || colorBlue === 'y') {
+  score++;
+  alert('Congrats, you have ' + score + ' points');
+} else {
+  alert('Sorry :(, better luck on the next one!');
+}
 
 var hairColor = prompt('Is my hair brown?').toLowerCase();
 console.log('hair color guess ' + hairColor);
-
-question(hairColor);
+if(hairColor === 'yes' || hairColor === 'y') {
+  score++;
+  alert('Congrats, you have ' + score + ' points');
+} else {
+  alert('Sorry :(, better luck on the next one!');
+}
 
 var hoodie = prompt('Are hoodies my favorite clothing?').toLowerCase();
 console.log('hoodie guess ' + hoodie);
-
-question(hoodie);
+if(hoodie === 'yes' || hoodie === 'y') {
+  score++;
+  alert('Congrats, you have ' + score + ' points');
+} else {
+  alert('Sorry :(, better luck on the next one!');
+}
 
 var videogame = prompt('Do I like videogames?').toLowerCase();
 console.log('videogame guess ' + videogame);
-
-question(videogame);
+if(videogame === 'yes' || videogame === 'y') {
+  score++;
+  alert('Congrats, you have ' + score + ' points');
+} else {
+  alert('Sorry :(, better luck on the next one!');
+}
 
 var dogs = prompt('Do I have dogs?').toLowerCase();
 console.log('dogs guess ' + dogs);
-
-question(dogs);
+if(dogs === 'yes' || dogs === 'y') {
+  score++;
+  alert('Congrats, you have ' + score + ' points');
+} else {
+  alert('Sorry :(, better luck on the next one!');
+}
