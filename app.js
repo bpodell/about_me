95da38b3d7e6945ad12a56f29fb3c503c21d3f10
Bill Odell <billodell@Bills-MacBook-Pro.local>
2017-10-03 19:54:06 -0700

    changed question 6 so that prompt shows up when out of guesses

--- app.js
+++ app.js
@@ -1,62 +1,62 @@
 'use strict';
 
 var score = 0;
-
-console.log('Initialized properly');
-alert('Welcome to my guessing game');
-
-var user = prompt('What is your name?');
-alert('Hi ' + user + ', this guessing game will teach you more about me, Bill, the programmer of this code! XD');
-console.log('users name ' + user);
-
-while(!user) {
-  user = prompt('name is required');
-}
-// Question 1
-var colorBlue = prompt('Is my favorite color blue?').toLowerCase();
-console.log('favorite color guess ' + colorBlue);
-if(colorBlue === 'yes' || colorBlue === 'y') {
-  score++;
-  alert('Congrats, you have ' + score + ' points');
-} else {
-  alert('Sorry :(, better luck on the next one!');
-}
-// Question 2
-var hairColor = prompt('Is my hair brown?').toLowerCase();
-console.log('hair color guess ' + hairColor);
-if(hairColor === 'yes' || hairColor === 'y') {
-  score++;
-  alert('Congrats, you have ' + score + ' points');
-} else {
-  alert('Sorry :(, better luck on the next one!');
-}
-// Question 3
-var hoodie = prompt('Are hoodies my favorite clothing?').toLowerCase();
-console.log('hoodie guess ' + hoodie);
-if(hoodie === 'yes' || hoodie === 'y') {
-  score++;
-  alert('Congrats, you have ' + score + ' points');
-} else {
-  alert('Sorry :(, better luck on the next one!');
-}
-// Question 4
-var videogame = prompt('Do I like videogames?').toLowerCase();
-console.log('videogame guess ' + videogame);
-if(videogame === 'yes' || videogame === 'y') {
-  score++;
-  alert('Congrats, you have ' + score + ' points');
-} else {
-  alert('Sorry :(, better luck on the next one!');
-}
-// Question 5
-var dogs = prompt('Do I have dogs?').toLowerCase();
-console.log('dogs guess ' + dogs);
-if(dogs === 'yes' || dogs === 'y') {
-  score++;
-  alert('Congrats, you have ' + score + ' points');
-} else {
-  alert('Sorry :(, better luck on the next one!');
-}
+//
+// console.log('Initialized properly');
+// alert('Welcome to my guessing game');
+//
+// var user = prompt('What is your name?');
+// alert('Hi ' + user + ', this guessing game will teach you more about me, Bill, the programmer of this code! XD');
+// console.log('users name ' + user);
+//
+// while(!user) {
+//   user = prompt('name is required');
+// }
+// // Question 1
+// var colorBlue = prompt('Is my favorite color blue?').toLowerCase();
+// console.log('favorite color guess ' + colorBlue);
+// if(colorBlue === 'yes' || colorBlue === 'y') {
+//   score++;
+//   alert('Congrats, you have ' + score + ' points');
+// } else {
+//   alert('Sorry :(, better luck on the next one!');
+// }
+// // Question 2
+// var hairColor = prompt('Is my hair brown?').toLowerCase();
+// console.log('hair color guess ' + hairColor);
+// if(hairColor === 'yes' || hairColor === 'y') {
+//   score++;
+//   alert('Congrats, you have ' + score + ' points');
+// } else {
+//   alert('Sorry :(, better luck on the next one!');
+// }
+// // Question 3
+// var hoodie = prompt('Are hoodies my favorite clothing?').toLowerCase();
+// console.log('hoodie guess ' + hoodie);
+// if(hoodie === 'yes' || hoodie === 'y') {
+//   score++;
+//   alert('Congrats, you have ' + score + ' points');
+// } else {
+//   alert('Sorry :(, better luck on the next one!');
+// }
+// // Question 4
+// var videogame = prompt('Do I like videogames?').toLowerCase();
+// console.log('videogame guess ' + videogame);
+// if(videogame === 'yes' || videogame === 'y') {
+//   score++;
+//   alert('Congrats, you have ' + score + ' points');
+// } else {
+//   alert('Sorry :(, better luck on the next one!');
+// }
+// // Question 5
+// var dogs = prompt('Do I have dogs?').toLowerCase();
+// console.log('dogs guess ' + dogs);
+// if(dogs === 'yes' || dogs === 'y') {
+//   score++;
+//   alert('Congrats, you have ' + score + ' points');
+// } else {
+//   alert('Sorry :(, better luck on the next one!');
+// }
 // Question 6
 var randInt = Math.random() * 10;
 var roundInt = Math.round(randInt);
@@ -75,6 +75,9 @@ for (var remainingGuess = 4; remainingGuess > 0; remainingGuess--) {
     score++;
     remainingGuess = 1;
   }
+}
+if( parse !== roundInt && remainingGuess === 0) {
+  alert('Sorry, out of guesses');
 }
 console.log('has this many remaining guesses for question 6 ' + remainingGuess);
 
