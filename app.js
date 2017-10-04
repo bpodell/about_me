49ea2a39e38e072e35bdd82dc5d7f2fb58288664
Bill Odell <billodell@Bills-MacBook-Pro.local>
2017-10-04 11:13:07 -0700

    changed question 7 to nestled for loop

--- app.js
+++ app.js
@@ -58,44 +58,49 @@ var score = 0;
 //   alert('Sorry :(, better luck on the next one!');
 // }
 // Question 6
-var randInt = Math.random() * 10;
-var roundInt = Math.round(randInt);
-console.log(roundInt);
-var ageGuess = prompt('Can you guess the number between 0 and 10?');
-for (var remainingGuess = 4; remainingGuess > 0; remainingGuess--) {
-  var parse = parseInt(ageGuess);
-  if(parse < roundInt) {
-    var ageGuess = prompt('Oops, too low... Try again, you have ' + remainingGuess + ' guesses remaining');
-    console.log('Number guessed was ' + ageGuess);
-  } else if (parse > roundInt){
-    var ageGuess =  prompt('Try a little lower... ' + remainingGuess + ' guesses remaining');
-    console.log('Number guessed was ' + ageGuess);
-  } else if (parse === roundInt){
-    alert('Correct!');
-    score++;
-    remainingGuess = 1;
-  }
-}
-if( parse !== roundInt && remainingGuess === 0) {
-  alert('Sorry, out of guesses');
-}
-console.log('has this many remaining guesses for question 6 ' + remainingGuess);
+// var randInt = Math.random() * 10;
+// var roundInt = Math.round(randInt);
+// console.log(roundInt);
+// var ageGuess = prompt('Can you guess the number between 0 and 10?');
+// for (var remainingGuess = 4; remainingGuess > 0; remainingGuess--) {
+//   var parse = parseInt(ageGuess);
+//   if(parse < roundInt) {
+//     var ageGuess = prompt('Oops, too low... Try again, you have ' + remainingGuess + ' guesses remaining');
+//     console.log('Number guessed was ' + ageGuess);
+//   } else if (parse > roundInt){
+//     var ageGuess =  prompt('Try a little lower... ' + remainingGuess + ' guesses remaining');
+//     console.log('Number guessed was ' + ageGuess);
+//   } else if (parse === roundInt){
+//     alert('Correct!');
+//     score++;
+//     remainingGuess = 1;
+//   }
+// }
+// if( parse !== roundInt && remainingGuess === 0) {
+//   alert('Sorry, out of guesses');
+// }
+// console.log('has this many remaining guesses for question 6 ' + remainingGuess);
 
 //Question 7
 var pixar = ['up','ratatouille','inside out'];
-var pixarGuess = prompt('Can you guess one of my favorite Pixar movies?').toLowerCase();
-console.log('Guess for question 7 ' + pixarGuess);
-for (var remainingGuess2 = 5; remainingGuess2 > 0; remainingGuess2--)
-  if(pixarGuess !== pixar[0] && pixarGuess !== pixar[1] && pixarGuess !== pixar[2]) {
-    var pixarGuess = prompt('I probably like that one, but its not one of my favorites, but you have ' + remainingGuess2 + ' guesses left to try again').toLowerCase();
-    console.log('Guess for question 7 ' + pixarGuess);
-  } else if(pixarGuess === pixar[0] || pixarGuess === pixar[1] || pixarGuess === pixar[2]){
-    alert('You got it!');
-    score++;
-    remainingGuess2 = 1;
+var remainingGuess2 = 6;
+var userIsCorrect = false;
+
+while (remainingGuess2 > 0) {
+  var pixarGuess = prompt('Can you guess one of my favorite Pixar movies?').toLowerCase();
+  remainingGuess2--;
+  for (var i = 0; i < pixar.length; i++){
+    if (pixarGuess === pixar[i]){
+      userIsCorrect = true;
+      score++;
+    }
   }
-console.log('This many remaining guesses ' + remainingGuess2);
-if(remainingGuess2 === 0) {
-  alert('My top three pixar movies are Up, Ratatouille, and Inside Out. :)');
+  if (userIsCorrect){
+    alert('You got it! My favorite pixar movies are Up, Ratatouille, and Inside Out');
+    break;
+  }else{
+    alert('sorry, try again!');
+  }
 }
+
 alert('Thank you so much for playing my game! You got ' + score + ' points out of a total 7. Great Job!');
